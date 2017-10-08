1568858f6d6a96ca3a1dc4c7c263cbef17e281ee
deadlywing <437743165@qq.com>
2017-10-08 10:06:04 +0800

    const_or_not

---
+++ const_or_not.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define SIZE 5
+
+void show_array(const double ar[], int n);
+void mult_array(double ar[], int n, double mult); // no const
+
+int main(void)
+{
+    double dip[SIZE] = {20.0, 17.66, 8.2, 15.3, 22.22};
+
+    printf("The original dip array : \n");
+    show_array(dip, SIZE);
+    mult_array(dip, SIZE, 2.5);
+    printf("The dip array now is : \n");
+    show_array(dip, SIZE);
+
+    return 0;
+}
+
+/* displays array contents , use const to inforce read-only*/
+void show_array(const double ar[], int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%8.3f ", ar[i]);
+    }
+    putchar('\n');
+}
+
+/* multiply each array element with same multipler */
+void mult_array(double ar[], int n, double mult)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        ar[i] *= mult;
+}
