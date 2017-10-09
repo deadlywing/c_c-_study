30fe5dad2862d270be79565ea7ee4be5a535f00b
deadlywing <437743165@qq.com>
2017-10-09 22:43:47 +0800

    nothing

--- String_Address/address_of_strings.c
+++ String_Address/address_of_strings.c
@@ -1,1 +1,24 @@
+/*
+ * @Author: zjj 
+ * @Date: 2017-10-09 22:33:51 
+ * @Last Modified by: zjj
+ * @Last Modified time: 2017-10-09 22:40:37
+ * @Description:  /
+*/
+
 #include <stdio.h>
+
+#define MSG "hhahahah"
+
+int main(void)
+{
+    char ar[] = MSG;
+    const char *pt = MSG;
+    printf("the address of MSG : %p  \n", "hhahahah");
+    printf("        address ar : %p  \n", ar);
+    printf("        address pt : %p  \n", pt);
+    printf("        address MSG : %p  \n", MSG);
+    printf("the address of MSG : %p  \n", "hhahahah");
+
+    return 0;
+}
